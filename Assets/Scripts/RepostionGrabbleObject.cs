88b4e87832673c144e2ffaf35a195535616bcca9
Mandar <99468218+MandarComXr@users.noreply.github.com>
2024-08-26 21:08:54 +0530

    First Push

---
+++ Assets/Scripts/RepostionGrabbleObject.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepostionGrabbleObject : MonoBehaviour
+{
+    private Vector3 myPosition;
+    private Vector3 myRotation;
+
+    private void Start()
+    {
+        this.myPosition = this.transform.position;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag(AnimHash.TOOL))
+        {
+            transform.position = myPosition;
+            transform.eulerAngles = myRotation;
+            Debug.Log("Debug");
+        }
+        else
+        {
+            Debug.Log("Debug 1");
+        }
+        
+    }
+
+
+
+}
